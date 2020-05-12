7c69528cdf8d2e25a71c92defda3de167c54be0f
李凯 <62530177+samurial@users.noreply.github.com>
2020-05-12 18:39:28 +0800

    数据库连接
    数据库连接

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connection = "server=localhost;user id=root;database=bbb;password=2411030483;SslMode = none;";
+            MySqlConnection conn = new MySqlConnection(connection);
+            conn.Open();
+            textBox1.Text="数据库连接成功";
+            conn.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
